450025d7c1d5f2b2d8164d8fe6df960a0ede882d
Kevin MM <kevinmm17@gmail.com>
2018-10-08 15:43:04 -0600

    PEP8 100%
    Verificación de PEP8 con Herramienta

--- forms.py
+++ forms.py
@@ -2,8 +2,19 @@ from wtforms import Form
 from wtforms import TextField
 from wtforms import validators
 
-""" Form de Datos para el CSV que recibe un Form de parametro con los datos ingresados por el cliente"""
+"""
+    Form de Datos para el CSV que recibe un Form
+    de parametro con los datos ingresados por el cliente
+"""
+
+
 class CommentForm(Form):
-    algoritmo = TextField('No. Algoritmo', [validators.Required(message = 'Ingrese un numero de algoritmo')])
-    comentario = TextField('Comentario', [validators.Required(message = 'Ingrese un comentario para el CSV')])
-    nombreCSV = TextField ('Nombre del CSV', [validators.Required(message = 'Ingrese un nombre para el CSV')])
+    algoritmo = TextField('No. Algoritmo',
+                          [validators.Required
+                           (message='Ingrese un numero de algoritmo')])
+    comentario = TextField('Comentario',
+                           [validators.Required
+                            (message='Ingrese un comentario para el CSV')])
+    nombreCSV = TextField('Nombre del CSV',
+                          [validators.Required
+                           (message='Ingrese un nombre para el CSV')])
--- run.py
+++ run.py
@@ -1,3 +1,15 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import send_from_directory
+import os
+import forms
+import imagen
+import pandas as pd
+
+UPLOAD_FOLDER = os.path.abspath("./uploads/")
+STATIC_FOLDER = os.path.abspath("./static/img")
+
 """
 @package docstring
 Aseguramiento de la Calidad del Software
@@ -9,34 +21,15 @@ Jose Daniel Delgado Segura - 2015001500
 
 """
 
-""" Imports para Front End """
-
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import send_from_directory
-
-import os
-
-UPLOAD_FOLDER = os.path.abspath("./uploads/")
-STATIC_FOLDER = os.path.abspath("./static/img")
-
-""" Imports para Macros y Objetos """
-
-import forms
-import imagen
-
-""" Imports para Manejo de CSV con pandas """
-import pandas as pd
-
 """"
     Crear Lista para manejo de imagenes en aplicacion web
     @param tam: tamanno de la lista que se desea construir
-    @return l: lista construida 
+    @return l: lista construida
 """
 
+
 def listaT(tam):
-    cont = 1;
+    cont = 1
     l = []
     if (tam == 0):
         raise ValueError('Lista de Imagenes Vacia')
@@ -55,26 +48,25 @@ def listaT(tam):
     @return nada
 """
 
+
 def escribirCSV(nombre, noAlgoritmo, comentario):
     """ Datos Default para prueba POC """
-    
     """ Diccionario de Datos ficticios que se agregaran al CSV """
-    
     if (nombre == "" or noAlgoritmo == "" or comentario == ""):
         raise ValueError('Todos los campos deben estar llenos')
-        
-    diccionario = {'Algorithm Number': [noAlgoritmo], 
-        'Obj_quantity': [24], 
-        'Precision': [42],
-        'Notes': [comentario]}
+    diccionario = {'Algorithm Number': [noAlgoritmo],
+                   'Obj_quantity': [24],
+                   'Precision': [42],
+                   'Notes': [comentario]}
 
     """ Columnas de la Estructura del CSV """
     listaColumnas = ['Algorithm Number', 'Obj_quantity', 'Precision', 'Notes']
-    
+
     """" Creando CSV """
-    df = pd.DataFrame(diccionario, columns = listaColumnas)
-    df.to_csv('C:/Users/Kevin MM/eclipse-workspace/SegmentacionCelulas/static/csv/'
-              +nombre+'.csv') #Especificar ruta
+    df = pd.DataFrame(diccionario, columns=listaColumnas)
+    df.to_csv('C:/Users/Kevin MM/eclipse-workspace/' +
+              'SegmentacionCelulas/static/csv/' +
+              nombre + '.csv')   # Especificar ruta
     print("Creado con Exito")
 
 """
@@ -85,51 +77,53 @@ def escribirCSV(nombre, noAlgoritmo, comentario):
     @return nada
 """
 
+
 def leerCSV(nombreArchivo):
-    df = pd.read_csv('C:/Users/Kevin MM/eclipse-workspace/SegmentacionCelulas/static/csv/'
-                     +nombreArchivo+'.csv')
-    print(df['Algorithm Number'][4] + ' ' + df['Precision'][4]) 
-    #Se escoge la columna a leer y lo demas se trata como una lista
+    df = pd.read_csv('C:/Users/Kevin MM/eclipse-workspace/' +
+                     'SegmentacionCelulas/static/csv/' +
+                     nombreArchivo + '.csv')
+    print(df['Algorithm Number'][4] + ' ' + df['Precision'][4])
+    # Se escoge la columna a leer y lo demas se trata como una lista
 
 
 """ Pagina Web """
 
 """ Nuevo Objeto """
-app = Flask(__name__) 
+app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.config["STATIC_FOLDER"] = STATIC_FOLDER
 
 """
-    Funcion para la llamada del archivo con los datos de la pagina web 
+    Funcion para la llamada del archivo con los datos de la pagina web
     @param nada
     @return nada
 """
 
 """ Decorador que contiene la ruta """
-@app.route('/', methods = ['GET', 'POST'])
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     """ Llamada al formulario de datos en la pagina web """
     comment_form = forms.CommentForm(request.form)
-    title = "Segmentacion de Celulas"   
-    
-    """ Si se recibe una solicitud POST de la paguina web y las validaciones 
-        funcionan se imprimen en pantalla y se genera el documento. Si no 
-        si se recibe una solicitud POST de la pagina y se tienen 
-        datos en la variable del input con el nombre imgUp 
+    title = "Segmentacion de Celulas"
+
+    """ Si se recibe una solicitud POST de la paguina web y las validaciones
+        funcionan se imprimen en pantalla y se genera el documento. Si no
+        si se recibe una solicitud POST de la pagina y se tienen
+        datos en la variable del input con el nombre imgUp
         se realiza la carga de dichas imagenes """
-        
-        
+
     if request.method == 'POST' and comment_form.validate():
-        print (comment_form.algoritmo.data)
-        print (comment_form.comentario.data)
-        print (comment_form.nombreCSV.data)
-        escribirCSV(comment_form.nombreCSV.data, comment_form.algoritmo.data, 
+        print(comment_form.algoritmo.data)
+        print(comment_form.comentario.data)
+        print(comment_form.nombreCSV.data)
+        escribirCSV(comment_form.nombreCSV.data, comment_form.algoritmo.data,
                     comment_form.comentario.data)
         print("CSV Generado")
 
-        
     elif request.method == "POST" and "imgUp" in request.files:
-        f= request.files.getlist("imgUp")
+        f = request.files.getlist("imgUp")
         fl = []
         # print(f)
         if f[0].filename != "":
@@ -139,25 +133,26 @@ def index():
                 send_from_directory(app.config["UPLOAD_FOLDER"], fn.filename)
                 fn.filename = "img/" + fn.filename
                 # print(fn.filename)
-                
+
                 # Se almacena en una lista para futuro procesamiento
                 item = imagen.Imagen(fn.filename)
                 fl = fl + [item]
-                
+
             print(f)
             ft = listaT(len(f))
             print("Imagenes Cargadas")
             # print ("Objeto Creado")
             # print (fl[0].getImagenes())
-            return render_template('index.html', title = title, 
-                                   form = comment_form, fp = f[0], filename= f[1:], ft = ft)
+            return render_template('index.html', title=title,
+                                   form=comment_form, fp=f[0],
+                                   filename=f[1:], ft=ft)
         else:
             raise ValueError('No se han seleccionado imagenes para procesar')
-        
+
     """ Renderiza pagina web para ser visualizada """
-    return render_template('index.html', title = title, form = comment_form)
+    return render_template('index.html', title=title, form=comment_form)
 
 """ Corre el servidor 8000 si lo dejo en default es 5000 """
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', debug= True, port= 8000)
+    app.run(host='0.0.0.0', debug=True, port=8000)
