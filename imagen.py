7eaef690e4f3976f5fbed4579228689d672a7e4f
Kevin MM <kevinmm17@gmail.com>
2018-10-15 04:56:19 -0600

    Enlace Modelo de Segmentación y App Web
    Enlace de las aplicaciones
    Solución de pulga en el modelo
    Adaptación del modelo para selección de archivos
    Despliegue en pantalla del resultado de segmentación

--- imagen.py
+++ imagen.py
@@ -4,6 +4,9 @@ class Imagen:
 
     def getImagenes(self):
         return self.ruta
+    
+    def getNombre(self):
+        return self.ruta[4:-4]
 
     def setImagenes(self, ruta):
         self.ruta = ruta
